2c41f575d5fd6190a53fb88180c9e1d6cf41d9a1
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-20 17:22:30 -0600

    Create new todo item input

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { format } from "date-fns";
+import { FaPlus } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className="flex justify-center pt-40">
       <div className="max-w-sm w-full shadow-lg bg-white p-8 rounded-xl opacity-90">
+        {/* Main Container */}
         <div className="flex justify-center cursor-default bg-gray-200 rounded-3xl px-4 py-1 color-gray hover:scale-110 transition-all">
           <img
             className="object-cover rounded-full w-16 h-16 m-2"
@@ -14,6 +16,19 @@ export default function Home() {
             <p className="text-3xl text-gray-600">Todo List</p>
             <p className="text-sm">{format(new Date(), "MMMM d, yyyy")}</p>
           </div>
+        </div>
+        {/* New Todo Item */}
+        <div className="relative mt-10">
+          <div className="absolute inset-y-0 left-2 flex items-center pl-3 pointer-events-none">
+            <FaPlus size={16} />
+          </div>
+          <input
+            type="text"
+            id="newTodo"
+            value=""
+            className="black w-full pl-10 p-2 border-4 rounded-full bg-gray-600 text-white"
+            placeholder="new todo item"
+          />
         </div>
       </div>
     </div>
